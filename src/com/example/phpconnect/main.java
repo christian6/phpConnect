9e182bec6ecc6c7aa8b85852d5afd7d5a91f79a4
Christian Valdez <cvaldezchavez@gmail.com>
2013-09-30 18:02:47 -0500

    Code Base

---
+++ src/com/example/phpconnect/main.java
@@ -0,0 +1,110 @@
+package com.example.phpconnect;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class main extends Activity {
+
+	 TextView t = (TextView) findViewById(R.id.text1);
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String uri = "http://172.16.0.80/app-android/app.php";
+        new httpcarga().execute(uri);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    private static String convertStreamToString(InputStream is) {
+    	/*
+    	* To convert the InputStream to String we use the
+    	* BufferedReader.readLine() method. We iterate until the BufferedReader
+    	* return null which means there's no more data to read. Each line will
+    	* appended to a StringBuilder and returned as String.
+    	*/
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    	StringBuilder sb = new StringBuilder();
+
+    	String line = null;
+    	try {
+	    	while ((line = reader.readLine()) != null) {
+	    		sb.append(line + "\n");
+	    	}
+	    	} catch (IOException e) {
+	    		e.printStackTrace();
+	    	} finally {
+		    	try {
+		    		is.close();
+		    	} catch (IOException e) {
+		    		e.printStackTrace();
+		    	}
+		    	return sb.toString();
+	    	}
+    }
+    	
+     public String httpstatus(String url){
+    	 HttpClient client = new DefaultHttpClient();
+    	 HttpPost post = new HttpPost(url);
+    	 String result = "";
+    	 try {
+    	 HttpResponse response = client.execute(post);
+    	 StatusLine statusLine = response.getStatusLine();
+    	 
+    	 if (statusLine.getStatusCode() == 200) {
+    	   HttpEntity entity = response.getEntity();
+    	   InputStream instream = entity.getContent();
+    	   result = convertStreamToString(instream);
+    	   instream.close();
+    	 } 
+    	 
+    	 
+    	 } catch (Exception e) {
+    	   result = "ERROR";
+    	 }
+		return result;
+
+
+    }
+    
+    class httpcarga extends AsyncTask<String,String,String>{
+    	protected void onPreExecute(){
+    		t.setText("Cargando Http");
+    	}
+    	
+    	protected String doInBackground(String... urls){
+    		return httpstatus(urls[0]);
+    	}
+    	
+    	protected void onPostExecute(String result){
+    		t.setText( result );
+    	}
+
+    }
+    
+}
+
+
